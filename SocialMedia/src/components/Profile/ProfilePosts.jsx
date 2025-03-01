e2769345e51cb7899c0cf9cff1c26f7da71b4b0a
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-03-02 00:37:10 +0530

    Profile Posts

--- SocialMedia/src/components/Profile/ProfilePosts.jsx
+++ SocialMedia/src/components/Profile/ProfilePosts.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { Box, Grid, VStack } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { Skeleton } from '../ui/skeleton';
+import ProfilePost from './ProfilePost';
+
 
 const ProfilePosts = () => {
+  const [isLoading,setIsLoading] = useState(true);
+  
+
+  useEffect(()=>{
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  },[])  
   return (
-    <div>ProfilePosts</div>
+    <>
+        <Grid
+        templateColumns={{
+        base: 'repeat(1, 1fr)',
+        md: 'repeat(3, 1fr)',
+        }}
+        gap={1}
+        columnGap={1}
+        >
+          {isLoading && [0,1,2,3,4,4].map((_,idx)=>(
+            <VStack key={idx} alignItems={'flex-start'} gap={4}>
+              <Skeleton w={'full'}>
+                <Box h={'300px'}>Contents wrapped</Box>
+              </Skeleton>
+            </VStack>
+          ))}
+
+          {!isLoading && (
+            <>
+            <ProfilePost img="/jack.avif"/>
+            <ProfilePost img="/post2.avif"/>
+            <ProfilePost img="/post3.avif"/>
+            <ProfilePost img="/alice.avif"/>
+            </>
+          )}
+        </Grid>
+    </>
   )
 }
 
